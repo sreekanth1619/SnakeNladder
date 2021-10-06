cee430d817f54eb19c5d1d9594439793b2dfcaa7
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-06 08:32:26 +0530

    Adding: exact position of the winning

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,32 +3,35 @@ package sankeandladder;
 import java.util.Random;
 
 public class SnakeAndLadder {
-	 public static void main(String[] args) {
-	        int position = 0;
-	        int IS_LADDER = 2;
-	        int IS_SNAKE = 1;
-	        int NO_PLAY = 0;
-	        int WINNING_POSITION = 100;
-	        System.out.println("Welcome to Snake and Ladder Game");
+	public static void main(String[] args) {
+        int position = 0;
+        int IS_LADDER = 2;
+        int IS_SNAKE = 1;
+        int NO_PLAY = 0;
+        int WINNING_POSITION = 100;
+        System.out.println("Welcome to Snake and Ladder Game");
 
-	        while (position < WINNING_POSITION) {
-	            Random random = new Random();
-	            int diceNumber = random.nextInt(6) + 1;
-	            int option = random.nextInt(3);
-	            if (option == IS_LADDER) {
-	                System.out.println("Ladder for the Player");
-	                position += diceNumber;
-	            } else if (option == IS_SNAKE) {
-	                System.out.println("Snake for the Player");
-	                position -= diceNumber;
-	                if (position < 0) {
-	                    position = 0;
-	                }
-	            } else {
-	                System.out.println("No Play for the Player");
-	            }
-	            System.out.println("Dice Number : " + diceNumber);
-	            System.out.println("Position : " + position);
-	        }
-	    }
+        while (position < WINNING_POSITION) {
+            Random random = new Random();
+            int diceNumber = random.nextInt(6) + 1;
+            int option = random.nextInt(3);
+            if (option == IS_LADDER) {
+                System.out.println("Ladder for the Player");
+                position += diceNumber;
+                if (position > 100){
+                    position -= diceNumber;
+                }
+            } else if (option == IS_SNAKE) {
+                System.out.println("Snake for the Player");
+                position -= diceNumber;
+                if (position < 0) {
+                    position = 0;
+                }
+            } else {
+                System.out.println("No Play for the Player");
+            }
+            System.out.println("Dice Number : " + diceNumber);
+            System.out.println("Position : " + position);
+        }
+    }
 }
